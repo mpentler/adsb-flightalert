4b88fffdd40319dae185b470a143eee4df16ac13
Mark Pentler <tehhustler@hotmail.com>
2022-11-29 18:53:36 +0000

    multiple filter support added! also tidied up some code comments and made cosmetic chsnges to outputs reflecting the new filter code

--- adsbflightalert.py
+++ adsbflightalert.py
@@ -1,9 +1,18 @@
-# adsb-flightalert 0.3 - main library
+# adsb-flightalert 0.4 - main library
 # parse a running dump1090-whatever install's aircraft.json for a particular item in a key
 # for example squawk = 7700, flight = blah, etc
 import json
 
-def parseJSONfile(aircraft_json_path, filter_text, filter_type):
+def parseJSONfile(aircraft_json_path, filters):
+  """
+  Returns a dict of dicts of aircraft matching the filter, with a
+  count in [0], or returns 0 if there aren't any matching
+
+  aircraft_json_path (str): full path to aircraft.json with
+    trailing slash
+  filters (tuple): filter tuples in form ("type", "text")
+  """
+
   current_alert_count = 0
   current_plane_count = 0
   alerted_aircraft_list = {} # dict to store any aircraft matching the filter
@@ -14,13 +23,18 @@ def parseJSONfile(aircraft_json_path, filter_text, filter_type):
     for tracked_aircraft in live_data['aircraft']:
       current_plane_count += 1
 
-      if filter_type in tracked_aircraft:
-        if filter_text in tracked_aircraft[filter_type]:
-          current_alert_count += 1
-          if 'flight' not in tracked_aircraft: # Fix blank callsigns
-            tracked_aircraft['flight'] = "NONE"
-          alerted_aircraft_list[current_alert_count] = tracked_aircraft # add aircraft to alerted dict
-          logAlert(tracked_aircraft)
+      for filter in filters:
+        filter_type = filter[0]
+        filter_text = filter[1]
+
+        if filter_type in tracked_aircraft:
+          if filter_text in tracked_aircraft[filter_type]:
+            current_alert_count += 1
+            if 'flight' not in tracked_aircraft: # Fix blank callsigns
+              tracked_aircraft['flight'] = "NONE"
+            tracked_aircraft['matched'] = filter_type
+            alerted_aircraft_list[current_alert_count] = tracked_aircraft # add aircraft to alerted dict
+            logAlert(tracked_aircraft)
 
   if (current_alert_count > 0):
     alerted_aircraft_list[0] = current_alert_count # set alerted planes count in dict
@@ -29,4 +43,11 @@ def parseJSONfile(aircraft_json_path, filter_text, filter_type):
     return 0
 
 def logAlert(alerted_aircraft): # send to stdout
-  print("Alert for - Hex:", alerted_aircraft['hex'], "Squawk:", alerted_aircraft['squawk'], "Callsign:", alerted_aircraft['flight'])
+  """
+  Logs an alerted aircraft to stdout
+
+  alerted_aircraft (dict): the alerted aircraft's record
+  """
+  # add time/date to log? if running as service there is no need
+  # if not then it would be useful. can i detect which somehow?
+  print(alerted_aircraft['matched'], "alert for - Hex:", alerted_aircraft['hex'], "Squawk:", alerted_aircraft['squawk'], "Callsign:", alerted_aircraft['flight'])
