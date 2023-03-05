0551b1f854dd756a6ec7f878b0b7a7d880dc4e23
Mark Pentler <tehhustler@hotmail.com>
2023-03-05 15:27:05 +0000

    tidied up the readme for 0.5, and a small code tweak to suppress alerts on second trigger

--- adsbflightalert.py
+++ adsbflightalert.py
@@ -1,53 +1,76 @@
-# adsb-flightalert 0.4 - main library
-# parse a running dump1090-whatever install's aircraft.json for a particular item in a key
+# adsb-flightalert 0.5 - main library
+# parse a running dump1090-whatever install's aircraft.json for particular criteria in a key
 # for example squawk = 7700, flight = blah, etc
 import json
 
 def parseJSONfile(aircraft_json_path, filters):
   """
   Returns a dict of dicts of aircraft matching the filter, with a
-  count in [0], or returns 0 if there aren't any matching
+  count of entries in dict[0], or returns 0 if there aren't any matching
 
   aircraft_json_path (str): full path to aircraft.json with
     trailing slash
-  filters (tuple): filter tuples in form ("type", "text")
+  filters (list): filter tuples in form ("type", "text"),... etc
   """
 
   current_alert_count = 0
-  current_plane_count = 0
+  inside_box_count = 0
   alerted_aircraft_list = {} # dict to store any aircraft matching the filter
 
   with open(aircraft_json_path + 'aircraft.json', 'r') as datafile:
     live_data = json.load(datafile) # load live aircraft.json file
 
     for tracked_aircraft in live_data['aircraft']:
-      current_plane_count += 1
 
-      for filter in filters:
+      # fix any missing data in mode S returns
+      if 'flight' not in tracked_aircraft: # Fix blank callsign
+        tracked_aircraft['flight'] = "NONE"
+      if 'squawk' not in tracked_aircraft: # Fix blank squawk
+        tracked_aircraft['squawk'] = "XXXX"
+
+      for filter in filters: # iterate through the filters for each plane in tbe json
         filter_type = filter[0]
         filter_text = filter[1]
 
-        if filter_type in tracked_aircraft:
-          if filter_text in tracked_aircraft[filter_type]:
-            current_alert_count += 1
-            if 'flight' not in tracked_aircraft: # Fix blank callsigns
-              tracked_aircraft['flight'] = "NONE"
-            tracked_aircraft['matched'] = filter_type
-            alerted_aircraft_list[current_alert_count] = tracked_aircraft # add aircraft to alerted dict
-            logAlert(tracked_aircraft)
+        if filter_type != "area": # non-area filters
+          if filter_type in tracked_aircraft:
+            if filter_text in tracked_aircraft[filter_type]:
+              current_alert_count += 1
+              tracked_aircraft['matched'] = filter_type
+              alerted_aircraft_list[current_alert_count] = tracked_aircraft # add aircraft to alerted dict
+        else: # area filter
+          if 'lat' in tracked_aircraft:
+            if isInsideBox(filter_text, tracked_aircraft):
+              inside_box_count += 1
+              current_alert_count += 1
+              tracked_aircraft['matched'] = filter_type
+              alerted_aircraft_list[current_alert_count] = tracked_aircraft # add aircraft to inside-the-box dict
+
+  # todo: add support for more than one area filter?
+  if (inside_box_count > 0):
+    print(inside_box_count, "aircraft in user-defined box")
 
   if (current_alert_count > 0):
-    alerted_aircraft_list[0] = current_alert_count # set alerted planes count in dict
+    alerted_aircraft_list[0] = current_alert_count # set alerted planes count in dict. currently planes
+    # are counted twice if they are caught by two filters in the same pass
     return alerted_aircraft_list
   else:
     return 0
 
-def logAlert(alerted_aircraft): # send to stdout
+def isInsideBox(coords_box, tracked_aircraft):
   """
-  Logs an alerted aircraft to stdout
+  Checks to see if a passed aircraft's position is within the passed in
+  coordinates.
 
-  alerted_aircraft (dict): the alerted aircraft's record
+  coords_box (list): contains two tuples containing (x, y) coords
+  tracked_aircraft (dict): aircraft to check
   """
-  # add time/date to log? if running as service there is no need
-  # if not then it would be useful. can i detect which somehow?
-  print(alerted_aircraft['matched'], "alert for - Hex:", alerted_aircraft['hex'], "Squawk:", alerted_aircraft['squawk'], "Callsign:", alerted_aircraft['flight'])
+  plane_y = tracked_aircraft['lat']
+  plane_x = tracked_aircraft['lon']
+  y1 = coords_box[0][0]
+  y2 = coords_box[1][0]
+  x1 = coords_box[0][1]
+  x2 = coords_box[1][1]
+
+  b = (x1 < plane_x < x2) and (y1 < plane_y < y2)
+  return b
--- example.py
+++ example.py
@@ -1,44 +1,79 @@
-# adsb-flightalert 0.4 - example script
-# search for emergency squawks and log them to stdout
+# adsb-flightalert 0.5 - example script
+# parse a running dump1090-whatever install's aircraft.json for particular criteria in a key
+# for example squawk = 7700, flight = blah, etc
 import adsbflightalert
 import time
+import smtplib
+import socket
 
-alert = False # alert state
-already_alerted = False # have we triggered current alert already?
+def logAlerts(alerted_aircraft): # send to stdout
+  # add time/date to log? if running as service there is no need
+  # if not then it would be useful. can i detect which somehow?
+  num_of_alerts = alerted_aircraft[0]
 
-check_delay = 60 # how often to check in seconds
-filters = (("squawk","7700")) # tuple of tuples of filters, (type, text) - both strings in case of preceding 0s in the data
-aircraft_json_path = "/run/dump1090-fa/" # path to aircraft.json file
-
-print("==========")
-print("adsb-flightalert is listening every " + str(check_delay) + " seconds for:\n")
-for filter in filters:
-  print(filter[0], filter[1], sep=' | ')
-print("==========")
+  while num_of_alerts > 0:
+    print(alerted_aircraft[num_of_alerts]['matched'], "alert for - Hex:", alerted_aircraft[num_of_alerts]['hex'], "Squawk:", alerted_aircraft[num_of_alerts]['squawk'], "Callsign:", alerted_aircraft[num_of_alerts]['flight'])
+    num_of_alerts -= 1
 
 def main():
   global alert
-  global already_alerted
+  global already_alerting
+
+  start_time = time.time()
 
   while True:
-    scan_result = adsbflightalert.parseJSONfile(aircraft_json_path, filters)
-    if (scan_result != 0): # this is where you put your alert code!
-      # you could parse the returned dict to do more things here if you wanted
-      # key 0 contains the count of alerted planes
-      alert = True
-      if not already_alerted:
-        # fire off notificiations here
-        print("First trigger for alert")
-        already_alerted = True
-      else: # do something here to update a notification?
-        print("Continuing current alert")
-    else: # code for no alert state here, such as cancelling a notification if one exists
-      alert = False
-      if already_alerted: # check if this needs cancelling
-        print("Canceling alert")
-        already_alerted = False
+    current_time = time.time()
 
-    time.sleep(check_delay) # sleep until next check
+    if current_time > start_time + check_delay:
+      scan_result = adsbflightalert.parseJSONfile(aircraft_json_path, filters)
+
+      if (scan_result != 0): # this is where you put your alert code!
+        alert = True
+        if not already_alerting:
+          # fire off notificiations here
+          print("Triggering alert state")
+          already_alerting = True
+          logAlerts(scan_result) # display the alerted flights
+        else: # do something here to update a notification?
+         # you could check alerted hex codes against a list, for example,
+         # so you don't alert planes more than neccesary, only showing new planes,
+         # or skip all alert actions entirely if already in alert mode
+         print("Continuing current alert state")
+      else: # code for no alert state here, such as cancelling a notification if one exists
+        alert = False
+        if already_alerting: # check if this needs cancelling
+          print("Canceling alert state")
+          already_alerting = False
+          # unlight an LED, change a display, etc
+      start_time = current_time
+
+    time.sleep(1)
+
+alert = False # alert state
+already_alerting = False # have we triggered an alert already?
+check_delay = 10 # how often to check in seconds
+
+aircraft_json_path = "/run/dump1090-fa/" # path to aircraft.json file
+filters = [
+  ("squawk", "7500"),
+  ("squawk", "7600"),
+  ("squawk", "7700"),
+#  ("area", [(55.5, -4.0), (56.5, -3.3)]) # not used in example, but this is how you do an area check - a list of two tuplea
+]
+
+print("==========")
+print("adsb-flightalert is listening every " + str(check_delay) + " seconds for the following filters:\n")
+filter_count = 0
+for filter in filters:
+  filter_count += 1
+  print("Filter:", filter_count, filter[0], filter[1], sep=' | ')
+print("==========")
 
 if __name__ == "__main__":
+  try:
     main()
+  except KeyboardInterrupt:
+    print("==========")
+    print("adsb-flightalert is exiting...")
+    print("==========")
+    # perhaps you could cancel any alerts or output pins here to clean up before exit?
